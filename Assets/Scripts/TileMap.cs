6e329440c15403d218f28f007d791dfc25356ed4
Xen <klimovjeka0987@gmail.com>
2017-05-26 00:15:06 +0300

    Inst floors;

---
+++ Assets/Scripts/TileMap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour {
+
+    public int N;
+    public TileType[] tileTypes;
+	// Use this for initialization
+	void Start () {
+        int i = 0, j = 0;
+        float X = 0.5f, Y = 0.375f;
+        for (i = 0; i < N; i++)
+        {
+            for (j=0;j<N;j++)
+            {
+                Instantiate(tileTypes[0].tileVisualPrefab, new Vector3( (j-i)*X, (i+j)*(-Y), 1), Quaternion.identity);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
