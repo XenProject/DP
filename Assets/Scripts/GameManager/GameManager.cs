7f42ff4fcde4f15f30b40032248fd044565fa0b3
Xen <klimovjeka0987@gmail.com>
2017-05-28 07:18:57 +0300

    Added Dummy for Test; New Icon of Thematics;

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public int N;
+    public TileType[] tileTypes;//Типы пола
+    public WallType[] wallTypes;//Типы стен
+    public GameObject floorsMassive;//Объект на сцене для группировки полов
+    public GameObject wallsMassive;//Объект на сцене для группировки стен
+    public GameObject borderPrefab;
+    public GameObject DummyPrefab;
+
+    private float timer = 2.0f;
+    private GameObject dummy;
+    private float deg;
+    private float speed = 2.0f;
+	// Use this for initialization
+	void Start () {
+        dummy = Instantiate(DummyPrefab, new Vector3(0,0,-0.3f), Quaternion.identity);
+        Destroy(dummy,timer);
+        TileMap();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (dummy != null)
+        {
+            deg = Mathf.Deg2Rad * Random.Range(0.0f, 180.0f);
+            dummy.transform.position += new Vector3( speed*Time.deltaTime*Mathf.Cos(deg), speed*Time.deltaTime * Mathf.Sin(deg), 0.0f);
+        }
+        else
+        {
+            dummy = Instantiate(DummyPrefab, new Vector3(0, 0, -0.3f), Quaternion.identity);
+            Destroy(dummy, timer);
+        }
+	}
+
+    void TileMap()
+    {
+        int i = 0, j = 0;
+        float X = 0.5f, Y = 0.375f;
+        for (i = 0; i < N; i++)
+        {
+            for (j = 0; j < N; j++)
+            {
+                //Создаем клетку пола в нужной координате и делаем его дочерним
+                (Instantiate(tileTypes[0].tileVisualPrefab, new Vector3((j - i) * X, (i + j) * (-Y), 0), Quaternion.identity)).transform.parent = floorsMassive.transform;
+                //Аналогично для стен
+                //Правые
+                if (i == 0)
+                {
+                    (Instantiate(wallTypes[0].wallVisualPrefab, new Vector3((j * X) + X / 2.0f, (j * -Y) + Y / 2.0f, 0 - (j / 10.0f)), Quaternion.identity)).transform.parent = wallsMassive.transform;
+                }
+                //Левые
+                if (j == 0)
+                {
+                    (Instantiate(wallTypes[0].wallVisualPrefab, new Vector3(((-i) * X) - X / 2.0f, (i * -Y) + Y / 2.0f, 0 - (j / 10.0f)), new Quaternion(0, 180.0f, 0, 1.0f))).transform.parent = wallsMassive.transform;
+                }
+                if (i == N - 1)
+                {
+                    Instantiate(borderPrefab, new Vector3((j - i) * X - X / 2.0f, (i + j) * (-Y) - Y / 2.0f, 0), new Quaternion(0, 180.0f, 0, 1.0f));
+                }
+                if (j == N - 1)
+                {
+                    Instantiate(borderPrefab, new Vector3((j - i) * X + X / 2.0f, (i + j) * (-Y) - Y / 2.0f, 0), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
